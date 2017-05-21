257e4afb2796aff210b5b8155c6ed19e8d34056e
carlamendozadominguez <carlamendozadominguez@gmail.com>
2017-05-21 17:36:06 +0200

    Libreria ImageViewZoom

---
+++ app/src/main/java/com/example/carla/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.carla.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView imageView;
+    PhotoViewAttacher mAttacher;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        mAttacher = new PhotoViewAttacher(imageView);
+    }
+}
